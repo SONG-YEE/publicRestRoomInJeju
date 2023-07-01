f4c46c0c8da79b3d7c1105222b63efd649b80358
songyee <rizwell0222@gmail.com>
2023-07-01 23:19:20 +0900

    Initial Commit

---
+++ publicRestRoomInJeju/src/main/java/com/sideProject/publicRestRoomInJeju/PublicRestRoomInJejuApplication.java
@@ -0,0 +1,13 @@
+package com.sideProject.publicRestRoomInJeju;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublicRestRoomInJejuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PublicRestRoomInJejuApplication.class, args);
+	}
+
+}
---
+++ publicRestRoomInJeju/src/test/java/com/sideProject/publicRestRoomInJeju/PublicRestRoomInJejuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sideProject.publicRestRoomInJeju;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublicRestRoomInJejuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
